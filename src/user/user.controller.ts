e8976b398822ab5d88906ce559479a69bb25f4f1
seongho <jklbnm2002@naver.com>
2023-01-06 17:29:01 +0900

    추가: 36장 links api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,32 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
+@UseGuards(AuthGuard)
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard)
   @Get(`admin/ambassadors`)
   ambassadors() {
     return this.userService.find({ where: { is_ambassador: true } });
+  }
+
+  @Get(`ambassador/rankings`)
+  async rankings() {
+    const ambassador: User[] = await this.userService.find({
+      where: {
+        is_ambassador: true,
+      },
+      relations: ['orders', 'orders.order_items'],
+    });
+
+    return ambassador.map((a) => {
+      return {
+        name: a.name,
+        revenue: a.revenue,
+      };
+    });
   }
 }
